eb6bf0d83afdc7c80d907beb026272ea24d10ef9
Kevin Dooley <kevindooley11@hotmail.com>
2019-04-07 15:30:11 +0100

    Added in code to examine basic top level info of the data set

--- Learn_pandas.py
+++ Learn_pandas.py
@@ -14,3 +14,13 @@ print(df.head())
 
 #default displays last 3 rows of data set
 print(df.tail())
+
+#tells me what data type (i.e. floating no./integer/ string etc) stored in each column
+print(df.dtypes)
+
+#top level info of the index column (most left column)
+print(df.index)
+
+#gives a list of the column headings
+print(df.columns)
+
