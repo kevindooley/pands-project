6a80461dc2681f164c5edb6dc620faddb84116db
Kevin Dooley <kevindooley11@hotmail.com>
2019-04-07 14:56:46 +0100

    Learned how to display 1st 5 and last 3 rows of dataframe

--- Learn_pandas.py
+++ Learn_pandas.py
@@ -2,9 +2,15 @@
 import numpy as np
 import pandas as pd
 
-
+# imported iris dataset csv file
+# prints out the data set
 df = pd.read_csv("irisdataset.csv")
 print(df)
 
 
+# prints out first out first 5 rows of dataset (default)
+# input any number to get that many rows displayed
+print(df.head())
 
+#default displays last 3 rows of data set
+print(df.tail())
