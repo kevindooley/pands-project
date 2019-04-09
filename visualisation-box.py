7ebb090c5b0dbffd1177cdb9152bd765af9341b0
Kevin Dooley <kevindooley11@hotmail.com>
2019-04-09 23:05:21 +0100

    Created code for 4 box plots to show differences in sepal length & width and petal length & width between each species

--- visualisation-box.py
+++ visualisation-box.py
@@ -7,8 +7,31 @@ import pandas as pd
 
 df = pd.read_csv("irisdataset.csv")
 
-# set the backgrounf to a white grid
-sns.set(style="whitegrid")
+#Comparision of species sepal length
+# set the background to a white grid
+sns.set(style="whitegrid", palette="Set2")
 # using seaborn - box plot comparing 3 species sepal length
 sns.boxplot(x="species", y="sepal_length", data=df)
+plt.title("Comparision of different Iris species sepal length", fontsize=14, fontweight='bold')
+plt.show()
+
+#Comparision of species sepal width
+sns.set(style="whitegrid", palette="Set2")
+
+sns.boxplot(x="species", y="sepal_width", data=df)
+plt.title("Comparision of different Iris species sepal width", fontsize=14, fontweight='bold')
+plt.show()
+
+#Comparision of species petal width
+sns.set(style="whitegrid", palette="Set2")
+
+sns.boxplot(x="species", y="petal_width", data=df)
+plt.title("Comparision of different Iris species petal width", fontsize=14, fontweight='bold')
+plt.show()
+
+#Comparision of species petal length
+sns.set(style="whitegrid", palette="Set2")
+
+sns.boxplot(x="species", y="petal_length", data=df)
+plt.title("Comparision of different Iris species petal length", fontsize=14, fontweight='bold')
 plt.show()
