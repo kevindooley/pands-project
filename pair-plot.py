abdd8e745a0b2202a363d20983682cf4a6c713b1
Kevin Dooley <kevindooley11@hotmail.com>
2019-04-11 21:39:54 +0100

    Figured out how to do pairplot

--- pair-plot.py
+++ pair-plot.py
@@ -1,9 +1,17 @@
 
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 import numpy as np
 import pandas as pd
+import seaborn as sns
 df = pd.read_csv("irisdataset.csv")
 
-sns.pairplot(df,hue="species", palette="GnBu_d")
+
+#create a pair plot
+#markers - gives legend 
+#hue - determines which column in the data frame should be used for colour encoding
+sns.pairplot(df, hue="species", palette="husl", markers=["o", "s", "D"])
+
+#show plot
+
+plt.show()
 
